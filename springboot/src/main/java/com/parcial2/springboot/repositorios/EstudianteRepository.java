ead44baf02b578215978dbafb7e2ca6ee11cb0b2
Nahuel Aciar <142058238+NahuelA19@users.noreply.github.com>
2025-10-19 19:56:44 -0300

    Delete

--- springboot/src/main/java/com/parcial2/springboot/repositorios/EstudianteRepository.java
+++
@@ -1,9 +0,0 @@
-package com.parcial2.springboot.repositorios;
-
-import com.parcial2.springboot.entidades.Estudiante;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EstudianteRepository  extends JpaRepository<Estudiante,Long> {
-}
